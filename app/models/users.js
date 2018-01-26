a9a6b5b0d1eba85bcde65a13432d9a75eafec5bb
‘rifkegribenes’ <rifkegribenes@gmail.com>
2018-01-25 22:49:07 -0800

    adding twitter, fb, google authentication

--- app/models/users.js
+++ app/models/users.js
@@ -4,12 +4,40 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var User = new Schema({
+	local: {
+		id: String,
+		email: String,
+		password: String
+	},
 	github: {
 		id: String,
+		token: String,
 		displayName: String,
 		username: String,
-      publicRepos: Number
+    publicRepos: Number,
+    avatarUrl: String
 	},
+	facebook: {
+    id: String,
+    token: String,
+    email: String,
+    name: String,
+    avatarUrl: String
+  },
+  twitter: {
+    id: String,
+    token: String,
+    displayName: String,
+    username: String,
+    avatarUrl: String
+  },
+  google: {
+    id: String,
+    token: String,
+    email: String,
+    name: String,
+    avatarUrl: String
+   },
    nbrClicks: {
       clicks: Number
    }
