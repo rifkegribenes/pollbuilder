9b95c513f4cc3382a9ac2a5b71ef35ab13cf674e
rifkegribenes <rifkegribenes@gmail.com>
2018-01-26 22:44:07 -0800

    added packages for auth tutorial

--- app/config/passport.js
+++ app/config/passport.js
@@ -64,21 +64,77 @@ module.exports = function (passport) {
 	  })
   );
 
+  // ======================================================================
+  // FACEBOOK =============================================================
+  // ======================================================================
   passport.use(new FacebookStrategy({
-    clientID: configAuth.facebookAuth.clientID,
-    clientSecret: configAuth.facebookAuth.clientSecret,
-    callbackURL: configAuth.facebookAuth.callbackURL,
+      clientID: configAuth.facebookAuth.clientID,
+      clientSecret: configAuth.facebookAuth.clientSecret,
+      callbackURL: configAuth.facebookAuth.callbackURL,
+      passReqToCallback: true
   },
-  function(accessToken, refreshToken, profile, done) {
-    // asynchronous verification, for effect...
-    process.nextTick(function () {
 
-      // To keep the example simple, the user's Facebook profile is returned to
-      // represent the logged-in user. In a typical application, you would want
-      // to associate the Facebook account with a user record in your database,
-      // and return that user instead.
-      return done(null, profile);
-    });
-  }
-));
+  // facebook will send back the token and profile
+  function(req, token, refreshToken, profile, done) {
+
+    // asynchronous
+    process.nextTick(function() {
+
+      // check if the user is already logged in
+      if (!req.user) {
+
+        // find the user in the database based on their facebook id
+        User.findOne({ 'facebook.id': profile.id }, function(err, user) {
+
+          // if there is an error, stop everything and return that
+          if (err) return done(err);
+
+          // if the user is found, then log them in
+          if (user) {
+            return done(null, user); // user found, return that user
+          } else {
+            // if there is no user found with that facebook id, create them
+            var newUser = new User();
+
+            // set all of the facebook information in our user model
+            newUser.facebook.id = profile.id;
+            newUser.facebook.token = token;
+            newUser.facebook.name = profile.name.givenName + ' ' + profile.name.familyName;
+            newUser.facebook.email = profile.emails[0].value;
+
+            // save our user to the database
+            newUser.save(function(err) {
+              if (err)
+               	throw err;
+
+              // if successful, return the new user
+              return done(null, newUser);
+            });
+          }
+
+      });
+
+    } else {
+      // user already exists and is logged in, we have to link accounts
+      var user = req.user; // pull the user out of the session
+
+      // update the current user's facebook credentials
+      user.facebook.id = profile.id;
+      user.facebook.token = token;
+      user.facebook.name = profile.name.givenName + ' ' + profile.name.familyName;
+      user.facebook.email = profile.emails[0].value;
+
+      // save the user
+      user.save(function(err) {
+        if (err)
+          throw err;
+        return done(null, user);
+      });
+    }
+
+  });
+
+  }));
+
+
 };
