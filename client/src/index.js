56511a2cc08718f49bd4e422be8f14d882bbaced
rifkegribenes <rifkegribenes@gmail.com>
2018-01-30 22:56:22 -0800

    fixed react-router render issue

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import './styles/css/index.css';
-import App from './App';
-import store from './store/store';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import "./styles/css/index.css";
+import App from "./App";
+import store from "./store/store";
+import registerServiceWorker from "./registerServiceWorker";
 
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 
 render(
-	<Provider store={store}>
-		<App />
-	</Provider>,
-	root,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  root
 );
 
 registerServiceWorker();
