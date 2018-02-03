1c87fdbeaefcd27a55bbf19e6852f438bac050d3
rifkegribenes <rifkegribenes@gmail.com>
2018-02-02 17:20:01 -0800

    added nodemon inspect to start scripts

--- app/config/passport.js
+++ app/config/passport.js
@@ -19,17 +19,15 @@ module.exports = function(passport) {
 
   // serialize the user for the session
   passport.serializeUser(function(user, done) {
-      done(null, user.id);
+    done(null, user.id);
   });
 
   // deserialize the user
   passport.deserializeUser(function(id, done) {
-      User.findById(id, function(err, user) {
-          done(err, user);
-      });
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
   });
-
-  console.log(configAuth.githubAuth.callbackURL);
 
   // =========================================================================
   // github  =================================================================
