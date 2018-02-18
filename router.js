91cc39b57b31e05c2565191349b56fd4c2260f9b
rifkegribenes <rifkegribenes@gmail.com>
2018-02-18 11:54:27 -0800

    moved serialize/deserialize out of router.js

--- router.js
+++ router.js
@@ -25,14 +25,6 @@ module.exports = function (app) {
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.serializeUser(function (user, done) {
-    done(null, user);
-});
-
-passport.deserializeUser(function (user, done) {
-    done(null, user);
-});
-
 // app.get('/auth/facebook', AuthenticationController.loginFacebook);
 app.get('/auth/facebook', passport.authenticate(
      'facebook',
