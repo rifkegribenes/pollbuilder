c4057640944c62b06ef661baec1b15b71cd64144
rifkegribenes <rifkegribenes@gmail.com>
2018-03-27 12:20:56 -0700

    styled footer

--- client/src/containers/Footer.jsx
+++ client/src/containers/Footer.jsx
@@ -1,17 +1,28 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import logo from "../img/surveybot.svg";
+import github from "../img/github-gradient.svg";
 
 const Footer = props => (
   <footer className="footer">
-    &copy; 2018 Sarah Schneider
+    <div className="footer__left">&copy; 2018</div>
+    <div className="footer__center">
+      <NavLink
+        to="/"
+        className="footer__logo-wrap"
+        activeClassName="footer__logo-wrap--active"
+      >
+        <img src={logo} className="footer__logo" alt="surveybot" />
+      </NavLink>
+    </div>
     <div className="footer__right">
       <a
         href="https://github.com/rifkegribenes/surveybot"
-        data-taborder=""
         rel="noopener noreferrer"
         target="_blank"
         className="footer__link"
       >
-        <i className="fa fa-github footer__icon" />
+        <img src={github} className="footer__icon" alt="github" />
       </a>
     </div>
   </footer>
