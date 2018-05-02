d914872e416208c23f2b3eeae92f7e0bceef7454
Glitch (pollbuilder) <none>
2018-05-02 18:40:14 +0000

    🎭🕶 Checkpoint ./app/config/auth.js:417995/234

--- app/config/auth.js
+++ app/config/auth.js
@@ -4,16 +4,16 @@ module.exports = {
 	'githubAuth': {
 		'clientID': process.env.GITHUB_KEY,
 		'clientSecret': process.env.GITHUB_SECRET,
-		'callbackURL': process.env.SERVER_URL + 'api/auth/github/callback/'
+		'callbackURL': 'https://pollbuilder.glitch.me/api/auth/github/callback'
 	},
 	'facebookAuth': {
 		'clientID': process.env.FACEBOOK_KEY,
 		'clientSecret': process.env.FACEBOOK_SECRET,
-		'callbackURL': process.env.SERVER_URL + 'api/auth/facebook/callback'
+		'callbackURL': 'https://pollbuilder.glitch.me/api/auth/facebook/callback'
 	},
 	'googleAuth': {
 		'clientID': process.env.GOOGLE_KEY,
 		'clientSecret': process.env.GOOGLE_SECRET,
-		'callbackURL': process.env.SERVER_URL + 'api/auth/google/callback'
+		'callbackURL': 'https://pollbuilder.glitch.me/api/auth/google/callback'
 	}
 };
