cd268a54ccf376da28f31b05fc0f01e8680d0e07
rifkegribenes <none>
2018-05-08 23:46:20 +0000

    Revert "💒🏙 Checkpoint"
    This reverts commit 9efc5cfacd8c7e0ebd6b485e27288d3cc803cddd.

--- client/src/containers/ViewPoll.jsx
+++ client/src/containers/ViewPoll.jsx
@@ -15,7 +15,6 @@ import PollCard from "./PollCard";
 class ViewPoll extends React.Component {
   componentWillMount() {
     const pollId = this.props.match.params.id;
-    console.log(window.SERVER_DATA;
     const currentPoll = window.SERVER_DATA.poll;
     this.props.actions.setCurrentPoll(currentPoll);
     // retrieve requested poll & save to app state
